5560670f95db18b0ef40e6de882f48e2107959ba
Rizal Solihin <ripisopol@gmail.com>
2024-06-12 16:41:09 +0700

    init

---
+++ src/main/java/com/riri/eventhop/EventhopApplication.java
@@ -0,0 +1,13 @@
+package com.riri.eventhop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventhopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhopApplication.class, args);
+	}
+
+}
