52a5bdbe59cce1427a0ce9da3e7b9555da3051d0
Rizal Solihin <ripisopol@gmail.com>
2024-06-25 09:00:26 +0700

    setup init

---
+++ src/main/java/com/riri/eventhop/EventhopApplication.java
@@ -0,0 +1,20 @@
+package com.riri.eventhop;
+
+import com.riri.eventhop.config.RsaKeyConfigProperties;
+import lombok.extern.java.Log;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@EnableCaching
+@EnableConfigurationProperties(RsaKeyConfigProperties.class)
+@Log
+public class EventhopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventhopApplication.class, args);
+
+	}
+}
