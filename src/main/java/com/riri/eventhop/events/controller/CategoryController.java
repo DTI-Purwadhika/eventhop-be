288c9507214fb75428725ddc8d9804cc6fa496c9
Rizal Solihin <ripisopol@gmail.com>
2024-06-30 23:55:18 +0700

    Implement response handling

--- src/main/java/com/riri/eventhop/events/controller/CategoryController.java
+++ src/main/java/com/riri/eventhop/events/controller/CategoryController.java
@@ -1,6 +1,8 @@
 package com.riri.eventhop.events.controller;
 
 import com.riri.eventhop.events.entity.EventCategory;
+import com.riri.eventhop.response.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,8 @@ import java.util.List;
 public class CategoryController {
 
     @GetMapping
-    public List<EventCategory> getAllCategories() {
-        return Arrays.asList(EventCategory.values());
+    public ResponseEntity<Response<List<EventCategory>>> getAllCategories() {
+        List<EventCategory> categories = Arrays.asList(EventCategory.values());
+        return Response.success("Categories retrieved successfully", categories);
     }
 }
