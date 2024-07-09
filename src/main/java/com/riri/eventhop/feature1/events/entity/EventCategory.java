92e3d3ac884f6aa3ffde62ab219aba327e9ec634
Rizal Solihin <ripisopol@gmail.com>
2024-07-09 16:38:40 +0700

    adjustment category

---
+++ src/main/java/com/riri/eventhop/feature1/events/entity/EventCategory.java
@@ -0,0 +1,5 @@
+package com.riri.eventhop.feature1.events.entity;
+
+public enum EventCategory {
+    ART, SCULPTING, TECHNOLOGY, MUSIC, BAKERY, WEALTH, FILM, DESIGN, BUSINESS, COOKING, DANCE, FASHION, LITERATURE, FITNESS, HEALTH, SCIENCE, TRAVEL, PHOTOGRAPHY, CRAFTS
+}
