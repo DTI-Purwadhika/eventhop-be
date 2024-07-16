16502bfd82c1236630c1bc3ee78b321c2ae4af64
Rizal Solihin <ripisopol@gmail.com>
2024-07-16 23:12:41 +0700

    init event analytics(revenue, attendee, and avg review) by period (daily, weekly, monthly, yearly)

--- src/main/java/com/riri/eventhop/feature1/tickets/TicketRepository.java
+++ src/main/java/com/riri/eventhop/feature1/tickets/TicketRepository.java
@@ -1,16 +1,20 @@
 package com.riri.eventhop.feature1.tickets;
 
-import com.riri.eventhop.feature1.tickets.Ticket;
-import com.riri.eventhop.feature2.users.entity.User;
+import com.riri.eventhop.feature1.reviews.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
+import java.math.BigDecimal;
+import java.time.Instant;
+
 
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+    @Query("SELECT COALESCE(SUM(t.price), 0) FROM Ticket t WHERE t.event.id = :eventId AND t.createdAt BETWEEN :startDate AND :endDate")
+    BigDecimal sumRevenueByEventIdAndDateRange(Long eventId, Instant startDate, Instant endDate);
 
+    @Query("SELECT COUNT(DISTINCT t.user.id) FROM Ticket t WHERE t.event.id = :eventId AND t.createdAt BETWEEN :startDate AND :endDate")
+    Integer countAttendeesByEventIdAndDateRange(Long eventId, Instant startDate, Instant endDate);
 }
+
--- src/main/java/com/riri/eventhop/feature2/users/auth/AuthService.java
+++ src/main/java/com/riri/eventhop/feature2/users/auth/AuthService.java
@@ -3,6 +3,8 @@ package com.riri.eventhop.feature2.users.auth;
 import com.riri.eventhop.feature2.users.entity.User;
 
 public interface AuthService {
+    String getCurrentUserId();
+    String getCurrentUserEmail();
     User findByEmail(String email);
     User save(User user);
 
